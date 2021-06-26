5dc057d3453437d5b4a95af02c774b9c9882c218
shimazaki-page <sk.03.19@gmail.com>
2021-06-26 19:58:15 +0900

    seeder first create

--- backend/database/seeders/SubjectAreasTableSeeder.php
+++ backend/database/seeders/SubjectAreasTableSeeder.php
@@ -15,5 +15,35 @@ class SubjectAreasTableSeeder extends Seeder
      */
     public function run()
     {
+        $subject_params = [
+            [
+                'subject' => '数学'
+            ],
+            [
+                'subject' => '英語'
+            ],
+            [
+                'subject' => '国語'
+            ],
+            [
+                'subject' => '理科'
+            ],
+            [
+                'subject' => '社会'
+            ]
+        ];
+
+        $class_params = array('1XS', '1XY', '2XS', '2XY', '3XS', '3XY');
+        $now = Carbon::now();
+
+        foreach ($subject_params as $subject_param) {
+            $subject_param['updated_at'] = $now;
+            $subject_param['created_at'] = $now;
+
+            foreach ($class_params as $class_param) {
+                $subject_param['classroom'] = $class_param;
+                DB::table('subject_areas')->insert($subject_param);
+            }
+        }
     }
 }
