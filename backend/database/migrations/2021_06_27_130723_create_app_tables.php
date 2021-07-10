a86a162eb495d0dbccd23967a2545fae67445e1d
shimazaki-page <sk.03.19@gmail.com>
2021-07-10 12:52:21 +0900

    migration作成

--- backend/database/migrations/2021_06_27_130723_create_app_tables.php
+++ backend/database/migrations/2021_06_27_130723_create_app_tables.php
@@ -13,22 +13,79 @@ class CreateAppTables extends Migration
      */
     public function up()
     {
-
-
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 20);
+            $table->string('name');
             $table->string('email')->unique('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('status',1);
             $table->rememberToken();
-            $table->foreignId('classroom_id')->references('id')->on('classrooms')
-                ->onDelete('cascade');
+            $table->integer('role')->default(10);
             $table->timestamps();
         });
 
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('class_name');
+        });
 
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject_name');
+        });
+
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('classroom_id')->references('id')->on('classroom')
+                ->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('student_subject', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->references('id')->on('students')
+                ->onDelete('cascade');
+            $table->foreignId('subject_id')->references('id')->on('subjects')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('homeworks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('homework');
+            $table->date('date');
+            $table->foreignId('classroom_id')->references('id')->on('classrooms')
+                ->onDelete('cascade');
+            $table->foreignId('subject_id')->references('id')->on('subjects')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->string('status')->default('-');
+            $table->foreignId('homework_id')->references('id')->on('homeworks')
+                ->onDelete('cascade');
+            $table->foreignId('student_id')->references('id')->on('students')
+                ->onDelete('cascade');
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('comment');
+            $table->string('image');
+            $table->foreignId('thread_id')->references('id')->on('threads')
+                ->onDelete('cascade');
+        });
     }
 
     /**
