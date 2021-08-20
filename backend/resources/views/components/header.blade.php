a34f36793f58c479e24fead5b0850d4de9cbedbd
shimazaki-page <sk.03.19@gmail.com>
2021-08-20 13:22:10 +0900

    fix logo link

--- backend/resources/views/components/header.blade.php
+++ backend/resources/views/components/header.blade.php
@@ -1,13 +1,18 @@
 <div class="header__container">
     <div class="header__contents--left">
-        <a href="{{route('top')}}" class="header__logo">HOMEWORKing</a>
+        @canany(['teacher','admin'])
+            <a href="{{route('top')}}" class="header__logo">HOMEWORKing</a>
+        @endcanany
+        @can('student')
+            <p class="header__logo">HOMEWORKing</p>
+        @endcan
     </div>
     <div class="header__contents--right">
         @auth
-                <form action="{{route('logout')}}" method="post">
-                    @csrf
-                    <input class="header__logout-button" type="submit" value="ログアウト">
-                </form>
+            <form action="{{route('logout')}}" method="post">
+                @csrf
+                <input class="header__logout-button" type="submit" value="ログアウト">
+            </form>
         @endauth
     </div>
 </div>
