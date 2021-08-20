dc2603417c445c12b5e10d9d6277cfe4a05ccac2
shimazaki-page <sk.03.19@gmail.com>
2021-08-20 13:02:01 +0900

    add pulldown month

--- backend/resources/views/students/status.blade.php
+++ backend/resources/views/students/status.blade.php
@@ -9,7 +9,25 @@
         <div class="page-title">宿題提出状況</div>
         <div class="status__title">
             <p class="status__title-left">教科 : {{$subject->subject_name}}</p>
-            <p>クラス : {{$classroom->class_name}}</p>
+            <p class="status__title-left">クラス : {{$classroom->class_name}}</p>
+            <div class="status__select-month">
+                <p class="status__title-left--month">月</p>
+                <select name="select-month" onchange="location.href=value;">
+                    <option >選択</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-01'])}}">1月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-02'])}}">2月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-03'])}}">3月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-04'])}}">4月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-05'])}}">5月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-06'])}}">6月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-07'])}}">7月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-08'])}}">8月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-09'])}}">9月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-10'])}}">10月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-11'])}}">11月</option>
+                    <option value="{{route('students.status',[$classroom->id,$subject->id,'2021-12'])}}">12月</option>
+                </select>
+            </div>
         </div>
         <table class="status__table">
             <tr class="status__column status__column--top">
